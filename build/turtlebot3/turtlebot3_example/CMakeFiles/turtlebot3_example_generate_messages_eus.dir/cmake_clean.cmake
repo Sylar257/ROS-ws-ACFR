469cfee55c7160af4685fb9385baab59b415ade9
Sylar257 <xgli001@e.ntu.edu.sg>
2019-08-20 10:05:32 +1000

    initial commit

---
+++ build/turtlebot3/turtlebot3_example/CMakeFiles/turtlebot3_example_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turtlebot3_example_generate_messages_eus"
+  "/home/sylar257/catkin_ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Goal.l"
+  "/home/sylar257/catkin_ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3ActionFeedback.l"
+  "/home/sylar257/catkin_ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Action.l"
+  "/home/sylar257/catkin_ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Feedback.l"
+  "/home/sylar257/catkin_ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3Result.l"
+  "/home/sylar257/catkin_ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3ActionGoal.l"
+  "/home/sylar257/catkin_ws/devel/share/roseus/ros/turtlebot3_example/msg/Turtlebot3ActionResult.l"
+  "/home/sylar257/catkin_ws/devel/share/roseus/ros/turtlebot3_example/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlebot3_example_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/turtlebot3/turtlebot3_example/CMakeFiles/turtlebot3_example_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turtlebot3_example_generate_messages_py"
+  "/home/sylar257/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3Goal.py"
+  "/home/sylar257/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionFeedback.py"
+  "/home/sylar257/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3Action.py"
+  "/home/sylar257/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3Feedback.py"
+  "/home/sylar257/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3Result.py"
+  "/home/sylar257/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionGoal.py"
+  "/home/sylar257/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionResult.py"
+  "/home/sylar257/catkin_ws/devel/lib/python2.7/dist-packages/turtlebot3_example/msg/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlebot3_example_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
